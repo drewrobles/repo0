9adc690417956540fdc448c80895e2b9cda195a9
Andrew Robles <andrewrobles@verizon.net>
2023-08-11 23:08:44 -0700

    use https in request url

--- weather/src/Weather.js
+++ weather/src/Weather.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react'
 
 function Weather () {
   const [data, setData] = useState('')
-  const baseUrl = 'http://api.openweathermap.org/data/2.5/weather'
+  const baseUrl = 'https://api.openweathermap.org/data/2.5/weather'
   const apiKey = 'b640c4354f674bcbe35d4c1f310d6a1a'
   useEffect(() => {
     const lat = 34.2783
