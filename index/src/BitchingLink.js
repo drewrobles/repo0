a5175bd20c326897799a9d3d4495e41673fb4236
Andrew Robles <andrewrobles@verizon.net>
2023-08-19 20:03:09 -0700

    feat: redirect to to hooks

--- index/src/BitchingLink.js
+++ index/src/BitchingLink.js
@@ -4,7 +4,7 @@ function RedirectExample() {
     useEffect(() => {
       const timeout = setTimeout(() => {
         // 👇️ redirects to an external URL
-        window.location.replace('https://open.spotify.com/user/21xh2ksi4wu4wl2z6mjsxia7a?si=7OdZA5swTXO-q0b8yP_P1A');
+        window.location.replace('https://hooks.andrewrobles.com');
       }, 0);
   
       return () => clearTimeout(timeout);
